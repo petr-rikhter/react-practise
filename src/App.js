54631884022dbe7b2a12780a72d35fe5a9b9fb32
Petr Rikhter <petr_rikhter@mail.ru>
2022-11-16 17:01:14 +0600

    added localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -7,13 +7,26 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //в данный эффект мы помещаем код побочного эффекта, то есть теперь весь этот код не будет запускаться при перезапуске компонента, он будет запускатьсяя автоматически реактом только в случае изменения зависимости в массиве.
+  useEffect(() => {
+    //создаем переменную где храним значение логин пользователя
+    const storedLoginInfo = localStorage.getItem("isLoggedIn");
+
+    //Вот так делать нельзя, так как в таком случае будет запускаться бесконечный цикл, так как при обновлении состояния обновляется компонент.
+    if (storedLoginInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
-    //Сохранение в локалсторейдж, первый аргумент - название(ключ), второй - значение
+    //Сохранение в локалсторейдж, первый аргумент - название(ключ), второй - значение. В коносли браузера его смотреть можно в application => local storage
     localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    //Очищаем локальное хранилище
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
